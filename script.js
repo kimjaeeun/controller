508d9a005045f957ea1093229c9fdea39fc78923
kimjaeeun <geeins55@naver.com>
2014-04-17 19:51:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,13 @@ $(document).ready(function(){
    $('#up-btn').click(function() {
     	$('#player').animate({top: "-=10px"}, 'fast');
     });
+    $('#down-btn').click(function() {
+    	$('#player').animate({top: "+=10px"}, 'fast');
+    });
+    $('#left-btn').click(function() {
+    	$('#player').animate({left: "-=10px"}, 'fast');
+    });
+    $('#right-btn').click(function() {
+    	$('#player').animate({left: "+=10px"}, 'fast');
+    });
 });
